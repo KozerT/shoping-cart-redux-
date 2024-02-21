ebead9ae67291653843a9e253cb33b5c242fcf85
KozerT <kozer.vizastaff@gmail.com>
2024-02-21 23:41:22 +0100

    feat: Introduced Header component and its corresponding styles.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 
 function App() {
   return (
     <>
+      <Layout />
       <Cart />
       <Products />
     </>
