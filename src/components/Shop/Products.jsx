48ed26846d1c3d3d2c0e83bf0a28b3a068e57b30
KozerT <kozer.vizastaff@gmail.com>
2024-02-22 23:59:15 +0100

    feat: Implemented quantity update functionality for cart items in Cart component using actions dispatch.

--- src/components/Shop/Products.jsx
+++ src/components/Shop/Products.jsx
@@ -1,16 +1,35 @@
 import classes from "./Products.module.css";
 import ProductItem from "./ProductItem";
 
+const DUMMY_PRODUCTS = [
+  {
+    id: "p1",
+    price: 6,
+    title: "Some Title",
+    description: "Some description",
+  },
+  {
+    id: "p2",
+    price: 9,
+    title: "Some Title2",
+    description: "Some description 2",
+  },
+];
+
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title="Test"
-          price={6}
-          description="This is a first product"
-        />
+        {DUMMY_PRODUCTS.map((product) => (
+          <ProductItem
+            key={product.id}
+            id={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          />
+        ))}
       </ul>
     </section>
   );
